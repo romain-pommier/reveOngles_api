d10f8a137c9d5f658c60bb81a5f92ba60498bed3
romain <romain-p31@hotmail.fr>
2021-05-06 14:20:18 +0200

    fix subCategory name for DB

--- database/migrations/2021_05_06_121944_create_sub_category.php
+++ database/migrations/2021_05_06_121944_create_sub_category.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class SubCategory extends Migration
+class CreateSubCategory extends Migration
 {
     /**
      * Run the migrations.
@@ -31,6 +31,6 @@ class SubCategory extends Migration
      */
     public function down()
     {
-        //
+        Schema::dropIfExists('sub_category');
     }
 }
